dd6a6d1d9cb91d6c255dff7f92d352f14e9f8459
Clarence Gomis <clarencegomis@MacBookPro.lan>
2026-02-27 03:06:32 +0100

    fix: move TrustLogos above UseCases section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
       <Hero />
       <Impact />
       <Approche />
-      <UseCases />
       <TrustLogos />
+      <UseCases />
       <WorkflowShowcase />
       <WhyNira />
       <SocialProof />
